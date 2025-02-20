d69d821bc8a0d2d1c788f74a529a0a23d56d1a8b
EC2 Default User <ec2-user@ip-172-31-1-93.ap-northeast-1.compute.internal>
2025-02-20 17:44:52 +0000

    [finish]4-21

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,6 +5,9 @@ belongs_to :user
 has_many :post_comments, dependent: :destroy
 has_many :favorites, dependent: :destroy
 
+validates :shop_name, presence: true
+validates :image, presence: true
+
 def get_image
   unless image.attached?
     file_path = Rails.root.join('app/assets/images/no_image.jpg')
