28d9a35c5c2604a9175d64071913a9e18d97727e
EC2 Default User <ec2-user@ip-172-31-1-93.ap-northeast-1.compute.internal>
2025-02-21 14:51:09 +0000

    [finish]4-26 meshiterro終了

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,5 +1,4 @@
 class PostCommentsController < ApplicationController
-
   def create
     post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
@@ -13,11 +12,9 @@ class PostCommentsController < ApplicationController
     redirect_to post_image_path(params[:post_image_id])
   end
   
-
   private
 
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
-  
 end
