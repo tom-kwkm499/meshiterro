982b6835fc865e79ea55d731c68a2b46dbd73a3c
EC2 Default User <ec2-user@ip-172-31-1-93.ap-northeast-1.compute.internal>
2025-03-02 14:51:25 +0000

    all

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
     post_images_path
   end
 
+
   def after_sign_out_path_for(resource)
     about_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:show, :edit, :update]
 
+ 
+
   
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
